f927e858b03496d1ee33481d7676e115e8e9870b
pancin <pancin@usi.ch>
2019-11-26 17:17:00 +0100

    added models for player and game

---
+++ backend/models/Game.js
@@ -0,0 +1,24 @@
+/** @constructor
+* @augments GameSchemaInstance
+* @param {Object} definition
+*/
+const mongoose = require('mongoose');
+const GameSchema = new mongoose.Schema(
+    {
+        activePlayer: { type: Number, required: true },
+        players: { type: Array, default: [] },
+        draw: { type: Number, required: true },
+        play: { type: Number, required: true },
+        maxHand: { type: Number, required: true },
+        maxKeepers: { type: Number, required: true },
+        rules: { type: Array, default: [] },
+        goal: {}, // definition required
+        discard: { type: Array, default: [] },
+        War: { type: Boolean, default: false }, //flag for goal
+        TV: { type: Boolean, default: false } //flag for goal
+    }
+);
+
+mongoose.model('Favorites', GameSchema);
+
+module.exports = GameSchema;
