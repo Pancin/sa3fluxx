7d6f5ad936f789c914ded8c2581ce25395bde7a0
pancin <pancin@usi.ch>
2019-12-01 16:27:37 +0100

    installed backend dependencies in package.json, added app.js for app setups, fectch.js for public and a first router in routes/root

---
+++ backend/routes/root/router.js
@@ -0,0 +1,16 @@
+/* */
+
+const express = require('express');
+const router = express.Router();
+const eventBus = require('../../bin/pubsub');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/fluxx', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+require('../../models/Card');
+const Card = mongoose.model('Card');
+require('../../models/Deck');
+const Deck = mongoose.model('Deck');
+require('../../models/Game');
+const Game = mongoose.model('Game');
+require('../../models/Player');
+const Player = mongoose.model('Player');
