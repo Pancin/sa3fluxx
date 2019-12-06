de004efefe3b7f15b9b208e62867a4d4f9531afd
pancin <pancin@usi.ch>
2019-12-06 12:06:00 +0100

    more routes

--- backend/routes/root/router.js
+++ backend/routes/root/router.js
@@ -30,7 +30,7 @@ let sendJSON = (res, status, obj) => {
 }
 
 //init and test purposes
-router.post('/all', (req,res) => {
+router.post('/all', (req, res) => {
     let testCard = new Card({
         name: 'test',
         filename: 'test.png',
@@ -39,59 +39,59 @@ router.post('/all', (req,res) => {
         type: 'test'
     });
     testCard.save()
-    .then(saved => {
-        console.log(saved);
-        let testGame = new Game({
-            deck: [],
-            activePlayer: 0,
-            players: [],
-            draw: 1,
-            play: 1,
-            playLeft: 1,
-            rules: [],
-            goal: {},
-            discard: [],
-            war: false,
-            tv: false,
-            silver: false,
-            richBonus: false,
-            poorBonus: false,
-            noHandBonus: false,
-            firstRandom: false,
-            lastRandom: false,
-            inflation: false
+        .then(saved => {
+            console.log(saved);
+            let testGame = new Game({
+                deck: [],
+                activePlayer: 0,
+                players: [],
+                draw: 1,
+                play: 1,
+                playLeft: 1,
+                rules: [],
+                goal: {},
+                discard: [],
+                war: false,
+                tv: false,
+                silver: false,
+                richBonus: false,
+                poorBonus: false,
+                noHandBonus: false,
+                firstRandom: false,
+                lastRandom: false,
+                inflation: false
+            });
+            return testGame.save();
+        })
+        .then(saved => {
+            console.log(saved);
+            let testDeck = new Deck({
+                size: 0,
+                cards: []
+            });
+            return testDeck.save();
+        })
+        .then(saved => {
+            console.log(saved);
+            let testPlayer = new Player({
+                order: 0,
+                name: 'testname',
+                isPlaying: false,
+                hand: [],
+                keepers: [],
+                creepers: [],
+                canWin: false
+            });
+            return testPlayer.save();
+        })
+        .then(saved => {
+            console.log(saved);
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
         });
-        return testGame.save();
-    })
-    .then(saved => {
-        console.log(saved);
-        let testDeck = new Deck({
-            size: 0,
-            cards: []
-        });
-        return testDeck.save();
-    })
-    .then(saved => {
-        console.log(saved);
-        let testPlayer = new Player({
-            order: 0,
-            name: 'testname',
-            isPlaying: false,
-            hand: [],
-            keepers: [],
-            creepers: [],
-            canWin: false
-        });
-        return testPlayer.save();
-    })
-    .then(saved => {
-        console.log(saved);
-        res.sendStatus(200);
-    })
-    .catch(err => {
-        console.log(err);
-        res.sendStatus(500);
-    });
 });
 
 router.get('/', (req, res) => {
@@ -103,15 +103,33 @@ router.get('/player/byName', (req, res) => {
     let filter = {};
     filter.name = req.body.name;
     Player.findOne(filter)
-    .then(found => {
-        if (!found) throw new Error('not found');
-        if (acceptJSON(req)) sendJSON(res, 200, found);
-        else res.sendStatus(200);
+        .then(found => {
+            if (!found) throw new Error('not found');
+            if (acceptJSON(req)) sendJSON(res, 200, found);
+            else res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(404);
+        });
+});
+
+router.put('/player/byName', (req,res) => {
+    let filter = {};
+    filter.name = req.body.name;
+    let update = {};
+    update.name = req.body.name;
+    update.order = req.body.order;
+    update.isPlaying = req.body.isPlaying;
+    update.hand = req.body.hand;
+    update.keepers = req.body.keepers;
+    update.creepers = req.body.creepers;
+    update.canWin = req.body.canWin;
+
+    Player.findOneAndUpdate(filter, update, {new:true})
+    .then(update => {
+        //do stuff
     })
-    .catch(err => {
-        console.log(err);
-        res.sendStatus(404);
-    });
 });
 
 //expects a body {url:<cardURL>}
@@ -122,41 +140,74 @@ router.get('/card/byURL', (req, res) => {
 
     filter.filename = filename;
     Card.findOne(filter)
-    .then(found => {
-        if (!found) throw new Error('not found');
-        if (acceptJSON(req)) sendJSON(res, 200, found);
-        else res.sendStatus(200);
-    })
-    .catch(err => {
-        console.log(err);
-        res.sendStatus(404);
-    });
+        .then(found => {
+            if (!found) throw new Error('not found');
+            if (acceptJSON(req)) sendJSON(res, 200, found);
+            else res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(404);
+        });
 });
 
-router.put('/deck/:deckId', (req,res) => {
+router.put('/card/byFilename', (req, res) => {
+    let filter = { filename: req.body.filename };
     let update = {};
-    update.size = req.body.size;
-    update.cards = req.body.cards;
+    update.filename = req.body.filename;
+    update.name = (req.body.name)?req.body.name:undefined;
+    update.dataURL = (req.body.dataURL)?req.body.dataURL:undefined;
+    update.description = (req.body.description)?req.body.description:undefined;
+    update.type = (req.body.type)?req.body.type:undefined;
 
-    Deck.findByIdAndUpdate(req.params.deckId, update, { new: true })
-    .then(found => {
-        if (!found) throw new Error('not found');
-        if (req.accepts('application/json')) sendJSON(res, 200, found);
+    Card.findOneAndUpdate(filter, update, {new: true})
+    .then(updated => {
+        if (!updated) throw new Error('not found');
+        if (acceptJSON(req)) sendJSON(res, 200, updated);
         else res.sendStatus(200);
     })
     .catch(err => {
         if (validContentType(req)) {
-            let newDeck = new Deck(req.body);
-            return newDeck.save()
+            let newCard = new Card(update);
+            newCard.save()
             .then(saved => {
-                if (acceptJSON(req)) sendJSON(res, 201, saved);
+                console.log(saved);
+                res.sendStatus(201);
             }).catch(err => {
                 console.log(err);
                 res.sendStatus(500);
             });
         }
-        else return null;
-    });
+        else {
+            res.sendStatus(400);
+        }
+    })
+});
+
+router.put('/deck/:deckId', (req, res) => {
+    let update = {};
+    update.size = req.body.size;
+    update.cards = req.body.cards;
+
+    Deck.findByIdAndUpdate(req.params.deckId, update, { new: true })
+        .then(found => {
+            if (!found) throw new Error('not found');
+            if (req.accepts('application/json')) sendJSON(res, 200, found);
+            else res.sendStatus(200);
+        })
+        .catch(err => {
+            if (validContentType(req)) {
+                let newDeck = new Deck(update);
+                return newDeck.save()
+                    .then(saved => {
+                        if (acceptJSON(req)) sendJSON(res, 201, saved);
+                    }).catch(err => {
+                        console.log(err);
+                        res.sendStatus(500);
+                    });
+            }
+            else return null;
+        });
 });
 
 // router.put('/game/:gameId', (req,res) => {
